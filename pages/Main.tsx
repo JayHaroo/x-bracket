5c57f60ebc370ec49c2a15250c3119f1fe16a0ac
JayHaroo <saturnomjey09@gmail.com>
2025-05-10 15:04:03 +0800

    feat: add navigation back button to Main page

--- pages/Main.tsx
+++ pages/Main.tsx
@@ -1,11 +1,19 @@
-import { View, Text, Pressable } from 'react-native';
+import { View, Text, Pressable } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 export default function Main() {
-    return (
-        <View className="flex-1 items-center justify-center bg-[#121212]">
-            <Pressable className="bg-[#1DB954] rounded-full px-4 py-2 mt-4">
-                <Text>Add a player</Text>
-            </Pressable>
-        </View>
-    )
-};
+  const navigation = useNavigation();
+  return (
+    <View className="flex-1 items-center justify-center bg-[#121212]">
+      <Pressable
+        className="bg-[#ce3636] rounded-full px-4 py-2 mt-4"
+        onPress={navigation.goBack}
+      >
+        <Text className="text-white p-1">Back</Text>
+      </Pressable>
+      <Pressable className="bg-[#1DB954] rounded-full px-4 py-2 mt-4">
+        <Text>Add a player</Text>
+      </Pressable>
+    </View>
+  );
+}
