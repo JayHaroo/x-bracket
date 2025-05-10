fb0f56a46ee96794e5abd3a474c29e609f962adc
JayHaroo <saturnomjey09@gmail.com>
2025-05-10 14:45:55 +0800

    feat: enhance Landing page with player addition button

--- pages/Landing.tsx
+++ pages/Landing.tsx
@@ -1,11 +1,12 @@
-import { View , Image, Text} from "react-native";
+import { View, Image, Text, Pressable } from "react-native";
 
 export default function Landing() {
-    return (
-        <View className="flex-1 items-center justify-center bg-[#121212]">
-        <Text className="text-2xl font-bold text-white mt-4">
-            XBracket
-        </Text>
-        </View>
-    );
-    }
+  return (
+    <View className="flex-1 items-center justify-center bg-[#121212]">
+      <Text className="text-2xl font-bold text-white mt-4">XBracket</Text>
+      <Pressable className="bg-[#1DB954] rounded-full px-4 py-2 mt-4">
+        <Text className="text-white p-3">Add players</Text>
+        </Pressable>
+    </View>
+  );
+}
