390a490890ca939ab9fe0b8adf44967825e96719
JayHaroo <saturnomjey09@gmail.com>
2025-05-10 15:01:51 +0800

    feat: implement navigation between Landing and Main pages, update dependencies

--- App.tsx
+++ App.tsx
@@ -1,14 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import "./global.css"
+import './global.css';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Landing from './pages/Landing';
+import Main from './pages/Main';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View className='flex-1'> 
-      <Landing />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Landing" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Landing" component={Landing} />
+        <Stack.Screen name="Main" component={Main} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
--- pages/Landing.tsx
+++ pages/Landing.tsx
@@ -1,11 +1,17 @@
 import { View, Image, Text, Pressable } from "react-native";
+import Main from "./Main";
+import { useNavigation } from "@react-navigation/native";
 
 export default function Landing() {
+  const navigation = useNavigation();
   return (
     <View className="flex-1 items-center justify-center bg-[#121212]">
       <Text className="text-2xl font-bold text-white mt-4">XBracket</Text>
       <Text className="text-white">Create Local Tournament Brackers!</Text>
-      <Pressable className="bg-[#1DB954] rounded-full px-4 py-2 mt-4">
+      <Pressable
+        className="bg-[#1DB954] rounded-full px-4 py-2 mt-4"
+        onPress={() => navigation.navigate("Main")}
+      >
         <Text className="text-white p-3">Create a tournamenr</Text>
       </Pressable>
       <Pressable className="bg-[#1d54b9] rounded-full px-4 py-2 mt-4">
