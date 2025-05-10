d3d34b7afa3bf683803b7fdc2d31e20fdd6f43fd
JayHaroo <saturnomjey09@gmail.com>
2025-05-10 14:41:51 +0800

    feat: implement Landing page and update Tailwind CSS configuration

---
+++ pages/Landing.tsx
@@ -0,0 +1,11 @@
+import { View , Image, Text} from "react-native";
+
+export default function Landing() {
+    return (
+        <View className="flex-1 items-center justify-center bg-[#121212]">
+        <Text className="text-2xl font-bold text-white mt-4">
+            XBracket
+        </Text>
+        </View>
+    );
+    }
