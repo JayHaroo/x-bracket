df436a8c34f049c1c32799544cfb3d1d37cd51ec
JayHaroo <saturnomjey09@gmail.com>
2025-05-10 14:50:37 +0800

    fix: correct spelling of "tournamenr" to "tournament" in Landing page

--- pages/Landing.tsx
+++ pages/Landing.tsx
@@ -4,6 +4,7 @@ export default function Landing() {
   return (
     <View className="flex-1 items-center justify-center bg-[#121212]">
       <Text className="text-2xl font-bold text-white mt-4">XBracket</Text>
+      <Text className="text-white">Create Local Tournament Brackers!</Text>
       <Pressable className="bg-[#1DB954] rounded-full px-4 py-2 mt-4">
         <Text className="text-white p-3">Create a tournamenr</Text>
       </Pressable>
