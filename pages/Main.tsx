883e198fe66851eb91ff1f971bdcf77b3abb6752
JayHaroo <saturnomjey09@gmail.com>
2025-05-10 15:12:25 +0800

    fix: add text color to "Add a player" button in Main page

--- pages/Main.tsx
+++ pages/Main.tsx
@@ -29,7 +29,7 @@ export default function Main() {
         <Text className="text-white p-1">Back</Text>
       </Pressable>
       <Pressable className="bg-[#1DB954] rounded-full px-4 py-2 mt-4">
-        <Text>Add a player</Text>
+        <Text className="text-white">Add a player</Text>
       </Pressable>
 
       {players.map((player, index) => (
