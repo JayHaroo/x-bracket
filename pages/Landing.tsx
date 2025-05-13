81e052b2214210cd7db5e24ee239af096b233430
JayHaroo <saturnomjey09@gmail.com>
2025-05-13 14:02:02 +0800

    feat: update Landing page styling and text for improved clarity and consistency

--- pages/Landing.tsx
+++ pages/Landing.tsx
@@ -5,13 +5,13 @@ export default function Landing() {
   const navigation = useNavigation();
   return (
     <View className="flex-1 items-center justify-center bg-[#121212]">
-      <Text className="text-5xl font-ShareTech text-white mt-4">XBracket</Text>
-      <Text className="text-white">Create Local Tournament Brackers!</Text>
+      <Text className="text-5xl font-Nippo text-white mt-4">XBracket</Text>
+      <Text className="text-xl text-white font-ShareTech">Create Local BBX Tournament Brackets!</Text>
       <Pressable
-        className="bg-[#1DB954] rounded-full px-4 py-2 mt-4"
+        className="border-2 border-[#09ff53] rounded-full px-4 py-2 mt-4"
         onPress={() => navigation.navigate("Main")}
       >
-        <Text className="text-white p-3">Create a tournamenr</Text>
+        <Text className="text-white p-3 text-[#09ff53]">Create a tournament</Text>
       </Pressable>
     </View>
   );
